09096bcd3f68f6376f763e1a353ec281a327315e
1992mm <qasimli.mm@gmail.com>
2024-06-05 17:48:23 +0400

    For loop guzgu formasi

--- src/az/atlacademy/tasks/TaskLoop.java
+++ src/az/atlacademy/tasks/TaskLoop.java
@@ -8,5 +8,21 @@ public class TaskLoop {
             }
             System.out.println("");
         }
+
+        for (int i = 0; i < 7; i++) {
+            for (int j = 6-i; j > 0 ; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+
+            }
+            System.out.println("");
+
+        }
+
     }
+
+
+
 }
