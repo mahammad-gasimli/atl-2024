534296f273f244971242aa2dee5b4af20a12b48b
1992mm <qasimli.mm@gmail.com>
2024-06-27 14:58:58 +0400

    Task  Random numbers

---
+++ src/az/atlacademy/tasks/array/ArrayTask.java
@@ -0,0 +1,35 @@
+package az.atlacademy.tasks.array;
+
+import java.util.Scanner;
+
+public class ArrayTask {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Arrayin olcusunu daxil edin");
+        int length = scanner.nextInt();
+        int [] massiv = new int[length];
+
+        massiv[0] = (int)(Math.random()*100);
+        int minumum = massiv[0];
+        for (int i = 1; i < massiv.length; i++) {
+            int random = (int)(Math.random()*100);
+            if (random< minumum){
+                minumum = random;
+                ArrayTask.class.getClassLoader();
+            }
+            massiv[i] = random;
+            System.out.print(massiv[i] + ", ");
+
+        }
+
+        System.out.println();
+        for (int i = 0; i < massiv.length; i++) {
+            if (massiv[i]%2==1){
+            System.out.println(i + "-ci " + massiv[i] + " element");}
+
+        }
+        System.out.println("Arreyde minumum eded " + minumum);
+
+
+    }
+}
